74d7ae9ad4ce851bf317d5b686506f37cb223ca1
Yash-Coder-07 <yash.rock1729@gmail.com>
2025-11-05 13:36:21 +0530

    Initial commit

---
+++ 6_Need_Array/oop6.java
@@ -0,0 +1,13 @@
+public class oop6 {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        System.out.println(nums[3]);
+        for(int i=0;i<nums.length;i++){
+            System.out.println(nums[i]);
+        }
+        for(int i=0;i<nums.length;i++){
+            System.out.println(nums[i]*2);
+        }
+    }    
+
+}
